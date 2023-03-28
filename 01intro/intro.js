99b9fbbb9f92a85e5ae5a7472dcc2a8a2fca4fc9
evgenii-shvetsov <sea.dev91@gmail.com>
2023-03-28 14:50:01 -0700

    practice ts syntax

--- 01intro/intro.js
+++ 01intro/intro.js
@@ -1,5 +1,3 @@
-const user = {name: "yev", age: 10};
-
+var user = { name: "yev", age: 10 };
 console.log("Hey hey");
 // console.log(user.email);
-
---
+++ 01intro/variables.ts
@@ -0,0 +1,40 @@
+let greetings: string = "Hello!";
+
+let mynum = 6;
+
+// greetings.toLowerCase();
+// console.log(greetings);
+
+let userId: number = 334455;
+
+let isLoggedIn: boolean = false;
+
+let hero: string;
+
+function getHero() {
+  return "daad";
+}
+
+hero = getHero();
+
+function addTwo(num: number) {
+  return num + 2;
+}
+
+addTwo(8);
+
+function getUpper(val: string) {
+  return val.toUpperCase();
+}
+
+function signUpUser(name: string, email: string, isPaid: boolean) {}
+
+let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
+
+loginUser("h", "h@h.da");
+
+signUpUser("yev", "at@at.com", false);
+
+getUpper("hey");
+
+export {};
